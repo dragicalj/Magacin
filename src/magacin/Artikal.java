cc72c5140e74a16738b482042bc627bdc2c563bd
dragicalj <gagaljubisavljevic111@gmail.com>
2022-03-15 18:33:47 +0100

    Napravljen projekat Magacin, apstraktna klasa Artikal, klase Knjiga,Kancelarijski materijal i Kucna hemija, generisane get i set metode za atribute

---
+++ src/magacin/Artikal.java
@@ -0,0 +1,36 @@
+package magacin;
+
+public class Artikal {
+	
+	protected int sifra;
+	protected String naziv;
+	protected String opis;
+	protected int kolicina;
+	public int getSifra() {
+		return sifra;
+	}
+	public void setSifra(int sifra) {
+		this.sifra = sifra;
+	}
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getOpis() {
+		return opis;
+	}
+	public void setOpis(String opis) {
+		this.opis = opis;
+	}
+	public int getKolicina() {
+		return kolicina;
+	}
+	public void setKolicina(int kolicina) {
+		this.kolicina = kolicina;
+	}
+	
+	
+	
+}
