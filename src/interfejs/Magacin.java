a320216a88fce5581dd589f08aa15ffd85a5a84f
dragicalj <gagaljubisavljevic111@gmail.com>
2022-03-15 19:47:57 +0100

    Dodata klasa Magacin koja implementira interfejs MagacinInterfejs

---
+++ src/interfejs/Magacin.java
@@ -0,0 +1,25 @@
+package interfejs;
+
+import magacin.Artikal;
+
+public class Magacin implements MagacinInterfejs{
+
+	@Override
+	public void dodajUMagacin(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbaciIzMagacina(Artikal artikal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Artikal pretraziArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
