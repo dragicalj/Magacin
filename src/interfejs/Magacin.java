49eb171275214f571bd104bf1076ba31712031d9
dragicalj <gagaljubisavljevic111@gmail.com>
2022-03-16 13:22:20 +0100

    Implementirane metode klase Magacin

--- src/interfejs/Magacin.java
+++ src/interfejs/Magacin.java
@@ -1,24 +1,54 @@
 package interfejs;
 
+import java.util.LinkedList;
+
 import magacin.Artikal;
 
 public class Magacin implements MagacinInterfejs{
+	
+	private LinkedList<Artikal> artikli=new LinkedList<>();
 
 	@Override
 	public void dodajUMagacin(Artikal artikal) {
-		// TODO Auto-generated method stub
+		if(artikal==null) {
+			throw new RuntimeException("Artikal ne sme biti null!");
+		}
+		for (Artikal artikalIzMagacina : artikli) {
+			if(artikalIzMagacina.equals(artikal)) {
+				artikalIzMagacina.setKolicina(artikalIzMagacina.getKolicina()+artikal.getKolicina());
+				return;
+			}
+		}
+		artikli.add(artikal);
 		
 	}
 
 	@Override
 	public void izbaciIzMagacina(Artikal artikal) {
-		// TODO Auto-generated method stub
-		
+		if(artikal==null) {
+			throw new RuntimeException("Artikal ne sme biti null!");
+		}
+		for (Artikal artikalIzMagacina : artikli) {
+			if(artikalIzMagacina.equals(artikal)) {
+				if(artikalIzMagacina.getKolicina()<artikal.getKolicina()) {
+					throw new RuntimeException("Nema dovoljno trazenog artikla na zalihama");
+				}
+				artikalIzMagacina.setKolicina(artikalIzMagacina.getKolicina()-artikal.getKolicina());
+			}
+		}
+		throw new RuntimeException("Ne postoji ovaj artikal");
 	}
 
 	@Override
 	public Artikal pretraziArtikal(int sifra) {
-		// TODO Auto-generated method stub
+		if(sifra<=0) {
+			throw new RuntimeException("Sifra mora biti veca od 0!");
+		}
+		for (Artikal pronadjeniArtikal : artikli) {
+			if(pronadjeniArtikal.getSifra()==sifra) {
+				return pronadjeniArtikal;
+			}
+		}
 		return null;
 	}
 
